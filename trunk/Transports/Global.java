3cfea60519385a95193d6e3509d6437bbdd574eb
lipixx <lipixx@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-12 18:10:17 +0000

    +Creada estructura de directoris inicial

---
+++ trunk/Transports/Global.java
@@ -0,0 +1,31 @@
+/* Aquesta classe conté les variables de l'enunciat*/
+package Transports;
+
+import java.util.*;
+
+public class Global {
+
+    /*Enunciat del problema*/
+    public static int H_INI = 8;
+    public static int H_FI = 17;
+    public static int HORES_SERVEI = H_FI - H_INI + 1;
+    public static int N_CENTRES = 6;
+
+    /*Tipus de Transports*/
+    public static int T1 = 500;
+    public static int T2 = 1000;
+    public static int T3 = 2000;
+    public static double preus_transport[] = { 100, 200, 300*1.5, 400*1.5, 500*2 };
+
+    /*Vector de peticions, conté N_CENTRES vectors, on
+     a cada vector hi ha les peticions del centre*/
+    public static Vector<Vector> PETICIONS;
+    
+    public Global()
+    {
+	for (int i=0; i<N_CENTRES; i++)
+	    {
+		PETICIONS.add(new Vector());
+	    }
+    }
+}
