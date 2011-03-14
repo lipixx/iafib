4574e62712b22ab3c7d6e85cbdc4aa2bdac6e1b7
lipixx <lipixx@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-14 11:20:53 +0000

    +Implementada l'Heuristica de Max. Falta provar-la.

--- Transports/Camio.java
+++ Transports/Camio.java
@@ -3,42 +3,58 @@ package Transports;
 import java.util.*;
 
 public class Camio {
-	
-	private int tipus;
-	private int carrega;
-	private ArrayList<Peticio> llista;
-	
-	public Camio (int t, int c, Peticio p)
-	{
-		tipus = t;
-		carrega = c;
-		llista = new ArrayList<Peticio>();
-		llista.add(p);
-	}
-	
-	public int getTipus()
-	{
-		return tipus;
-	}
-	
-	public int getCarrega()
-	{
-		return carrega;
-	}
-	
-	public void setCarrega(int c)
-	{
-		carrega = c;
-	}
-	
-	public ArrayList<Peticio> getLlistaPeticions()
-	{
-		return llista;
-	}
-	
-	public void addPeticio(Peticio p)
-	{
-		llista.add(p);
-		carrega += p.getQuantitat();
-	}
+    
+    private int tipus;
+    private int carrega;
+    private ArrayList<Peticio> llista;
+    
+    public Camio (int t, int c, Peticio p)
+    {
+	tipus = t;
+	carrega = c;
+	llista = new ArrayList<Peticio>();
+	llista.add(p);
+    }
+    
+    public int getTipus()
+    {
+	return tipus;
+    }
+    
+    public int getCarrega()
+    {
+	return carrega;
+    }
+    
+    public void setCarrega(int c)
+    {
+	carrega = c;
+    }
+    
+    public ArrayList<Peticio> getLlistaPeticions()
+    {
+	return llista;
+    }
+    
+    public void addPeticio(Peticio p)
+    {
+	llista.add(p);
+	carrega += p.getQuantitat();
+    }
+    public double getBeneficis(int horaCamio)
+    {
+	double benefici_total = 0;
+	for (int pet = 0; pet < llista.size(); pet++)
+	    {
+		Peticio actual = llista.get(pet);
+		double pMax = Global.preus_transport[actual.getQuantitat()/100-1];
+		int nHoresRetras = horaCamio - actual.getHoraLimit();
+
+		if (nHoresRetras < 0) nHoresRetras = 0;
+		
+		benefici_total = pMax - pMax *(nHoresRetras*0.2);	    
+	    }
+
+	return benefici_total;
+    }   
 }
