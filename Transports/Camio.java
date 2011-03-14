85fc4fca06bf43e1fa3203d711d147fbbc36c608
jordillamaspons <jordillamaspons@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-14 09:54:33 +0000

    + progressos amb generacio estat inicial

--- Transports/Camio.java
+++ Transports/Camio.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Camio {
 	
 	private int tipus;
-	private int capacitat;
+	private int carrega;
 	private ArrayList<Peticio> llista;
 	
 	public Camio (int t, int c, ArrayList<Peticio> ll)
 	{
 		tipus = t;
-		capacitat = c;
+		carrega = c;
 		llista = ll;
 	}
 }
