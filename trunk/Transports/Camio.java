7d92b207004c7bafa9875d4895673a505b889952
jordillamaspons <jordillamaspons@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-14 18:41:30 +0000

    + canvi de nom a alguna var de l'estat + correccio d'algun error de concepte a generacio estat inicial + afegir esquelet a estat per la 2a estrategia generadora de l'estat inicial + canviada la constructora de l'estat per reflectir el canvi anterior + per crear camio nou, passem tipus i petició; la constructora ja agafarà el pes de la peticio

--- trunk/Transports/Camio.java
+++ trunk/Transports/Camio.java
@@ -3,58 +3,59 @@ package Transports;
 import java.util.*;
 
 public class Camio {
-    
-    private int tipus;
-    private int carrega;
-    private ArrayList<Peticio> llista;
-    
-    public Camio (int t, int c, Peticio p)
-    {
-	tipus = t;
-	carrega = c;
-	llista = new ArrayList<Peticio>();
-	llista.add(p);
-    }
-    
-    public int getTipus()
-    {
-	return tipus;
-    }
-    
-    public int getCarrega()
-    {
-	return carrega;
-    }
-    
-    public void setCarrega(int c)
-    {
-	carrega = c;
-    }
-    
-    public ArrayList<Peticio> getLlistaPeticions()
-    {
-	return llista;
-    }
-    
-    public void addPeticio(Peticio p)
-    {
-	llista.add(p);
-	carrega += p.getQuantitat();
-    }
-    public double getBeneficis(int horaCamio)
-    {
-	double benefici_total = 0;
-	for (int pet = 0; pet < llista.size(); pet++)
-	    {
-		Peticio actual = llista.get(pet);
-		double pMax = Global.preus_transport[actual.getQuantitat()/100-1];
-		int nHoresRetras = horaCamio - actual.getHoraLimit();
+	
+	private int tipus;
+	private int carrega;
+	private ArrayList<Peticio> llista;
+	
+	//Creacio d'un camio amb un tipus i una peticio
+	public Camio (int t, Peticio p)
+	{
+		tipus = t;
+		carrega = p.getQuantitat();
+		llista = new ArrayList<Peticio>();
+		llista.add(p);
+	}
+	
+	public int getTipus()
+	{
+		return tipus;
+	}
+	
+	public int getCarrega()
+	{
+		return carrega;
+	}
+	
+	public void setCarrega(int c)
+	{
+		carrega = c;
+	}
+	
+	public ArrayList<Peticio> getLlistaPeticions()
+	{
+		return llista;
+	}
+	
+	public void addPeticio(Peticio p)
+	{
+		llista.add(p);
+		carrega += p.getQuantitat();
+	}
+	public double getBeneficis(int horaCamio)
+	{
+		double benefici_total = 0;
+		for (int pet = 0; pet < llista.size(); pet++)
+		{
+			Peticio actual = llista.get(pet);
+			double pMax = Global.preus_transport[actual.getQuantitat()/100-1];
+			int nHoresRetras = horaCamio - actual.getHoraLimit();
 
-		if (nHoresRetras < 0) nHoresRetras = 0;
+			if (nHoresRetras < 0) nHoresRetras = 0;
+			
+			benefici_total = pMax - pMax *(nHoresRetras*0.2);	    
+		}
 		
-		benefici_total = pMax - pMax *(nHoresRetras*0.2);	    
-	    }
-
-	return benefici_total;
-    }   
+		return benefici_total;
+	}   
 }
