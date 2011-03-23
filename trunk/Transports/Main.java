b0e22095f66c7477b35c437bccf461507afd89ec
lipixx <lipixx@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-23 12:28:55 +0000

    + Creades les funcions a Global.java que generen les peticions inicials del problema. + Afegida una funció a Matriu.java per pintar una matriu específica de peticions. + Afegit el codi a Main.java per inicialitzar el problema. La funció P.iniciaProblemaDefault(numPeticions,aleatori) s'encarrega de generar les peticions.

--- trunk/Transports/Main.java
+++ trunk/Transports/Main.java
@@ -14,34 +14,14 @@ public class Main
     {
 	//Init del problema
 	Global P = new Global();
-
-	//Hem de fer inputs d'usuari per definir el problema?
-	//Per defecte a la clase Global tenim tot l'enunciat i uns
-	//valors per defecte
-	Peticio p = new Peticio(1, 100, 9);
-	Peticio p1 = new Peticio(2, 200, 10);
-	Peticio p2 = new Peticio (3, 1000, 12);
-	Peticio p3 = new Peticio (4, 500,17);
+	P.iniciaProblemaDefault(-1,false);
 	
-	//PETICIONS.add(Hora_limit,Centre,Peticio)
-	P.PETICIONS.add(p2.getHoraLimit()-8,5,p2);
-	P.PETICIONS.add(p1.getHoraLimit()-8,5,p1);
-	P.PETICIONS.add(p.getHoraLimit()-8,5,p);
-	P.PETICIONS.add(p3.getHoraLimit()-8,5,p3);	
-	P.PETICIONS.print();	
-	
-	//Agafar llista de peticions d'una Hora i un Centre
-	ArrayList<Peticio> llista = P.PETICIONS.get(5,5);
-
-	System.out.println("Llista 5,5");
-	for (int i=0; i<llista.size(); i++)
-	    {
-		System.out.println(llista.get(i).getQuantitat());
-	    }
+	P.PETICIONS.printPeticions();	      
     }
+   
     /**Creacio problema amb HC amb funcio heuristica Max Guanys
-	 * @param gen1 Es true si fem servir estragia num 1 de generacio d'estat inicial, false si num 2
-	 */
+     * @param gen1 Es true si fem servir estragia num 1 de generacio d'estat inicial, false si num 2
+     */
     private static void TransportsHillClimbingSearchMaxGuanys(Matriu peticions, int n1, int n2, int n3, boolean gen1) {
 	System.out.println("\nTransports HillClimbing  -->");
 	try {
