959306f591888b8d3030ed24e5ef25e8f9f7c01b
lipixx <lipixx@7b7b1f08-6d03-dd83-1461-db8158f2a332>
2011-03-29 10:51:56 +0000

    + Indentació + Neteja de funcions inutils + Afegida opció per triar la generació de successors amb el paràmetre --hswap + Afegida la classe nova per els successors (la que hi havia canviada de nom) + Revisat el codi de Main , Estat, Camio i Successors.

--- Transports/TransportsMaxGuanysHeuristicFunction.java
+++ Transports/TransportsMaxGuanysHeuristicFunction.java
@@ -14,34 +14,34 @@ public class TransportsMaxGuanysHeuristicFunction implements HeuristicFunction {
         Estat state = (Estat) st;
         int beneficis = 0;
 
-	/*Obtenim les dues matrius de l'estat*/
+        /*Obtenim les dues matrius de l'estat*/
         Matriu camionsHCP = state.getCamionsHCP();
         Matriu endarrerits = state.getEndarrerits();
 
         /*Explorem tots els centres de la graella HCP*/
         for (int nc = 0; nc < Global.N_CENTRES; nc++) {
-	    /*Per cada centre, explorem totes les hores, n'obtenim el camió
-	      i li demanem a ell quin benefici porta.*/
+            /*Per cada centre, explorem totes les hores, n'obtenim el camió
+              i li demanem a ell quin benefici porta.*/
             for (int h = 0; h < Global.HORES_SERVEI; h++) {
                 Camio c = (Camio) camionsHCP.getObj(h,nc);
                 if (c!=null) beneficis += c.getBeneficis(h+Global.H_INI);
             }
 
             /*Ens queda explorar les peticions que no estan assignades a cap
-	      camió. El vector endarrerits només té una fila, la 0.*/
+            	      camió. El vector endarrerits només té una fila, la 0.*/
             ArrayList<Peticio> llistaEndar = endarrerits.get(0,nc);
 
             for (int pet = 0; pet < llistaEndar.size(); pet++) {
                 Peticio p = llistaEndar.get(pet);
-		/*Segons l'enunciat, hem de decrementar els beneficis una vegada pel benefici
-		 màxim que obtindriem amb la petició i un 20% sobre aquest preu tantes vegades
-		com hores de retràs.*/
-                beneficis -= (Global.preus_transport[p.getQuantitat()/100 -1] * 
-			      (Global.H_FI - p.getHoraLimit()) * 0.2) 
-		    + Global.preus_transport[p.getQuantitat()/100 -1];
+                /*Segons l'enunciat, hem de decrementar els beneficis una vegada pel benefici
+                 màxim que obtindriem amb la petició i un 20% sobre aquest preu tantes vegades
+                com hores de retràs.*/
+                beneficis -= (Global.preus_transport[p.getQuantitat()/100 -1] *
+                              (Global.H_FI - p.getHoraLimit()) * 0.2)
+                             + Global.preus_transport[p.getQuantitat()/100 -1];
             }
         }
-	/*L'algorisme final es basa en les pèrdues, multipliquem per tant per -1.*/
-	return beneficis * -1;
+        /*L'algorisme final es basa en les pèrdues, multipliquem per tant per -1.*/
+        return beneficis * -1;
     }
 }
--- Transports/TransportsSuccessorFunction.java
+++ Transports/TransportsSuccessorFunction.java
@@ -20,169 +20,80 @@ public class TransportsSuccessorFunction implements SuccessorFunction {
 
     public List getSuccessors(Object state) {
         Estat estatPare = (Estat) state;
-	Matriu hcpPare = estatPare.getCamionsHCP();
-	Matriu endarreritsPare = estatPare.getEndarrerits();
+        Matriu hcpPare = estatPare.getCamionsHCP();
+        Matriu endarreritsPare = estatPare.getEndarrerits();
 
-	List<Successor> successors = new ArrayList<Successor>();
+        List<Successor> successors = new ArrayList<Successor>();
 
-	/**
-	 * L'estratègia a seguir serà la següent:
-	 * Per un centre de producció CP
-	 * 1. Per cada Peticio pi de CP, que es troba a HCP
-	 *      Per totes les demés peticions pj tals que j > i (en ordre a la matriu HCP)
-	 *        swap (pi,pj);
-	 * 2. Per cada Petició pi de CP, que es troba a HCP
-	 *      Per totes les peticions pj de la matriu endarrerits (en ordre a la matriu)
-	 *	  swap (pi,pj);
-	 *
-	 * La condició de que j > i en (1), implica que no es repeteixen estats.
-	 */
-	
-	for (int cp = 0; cp < Global.N_CENTRES; cp++)
-	    {
-		for (int fila = 0; fila < Global.HORES_SERVEI; fila++)
-		    {			
-			Camio c = (Camio) hcpPare.getObj(fila,cp);
-			if (c != null)
-			    {
-				ArrayList<Peticio> llistaCamio = c.getLlistaPeticions();
-				/*Del camio c, i de la i del seguent for, traurem el pi*/
-				for (int i = 0; i < llistaCamio.size(); i++)
-				    {
-					/*La petició pi és dins la HCP[fila][cp]->c->llistaCamio[i]*/
-					Peticio pi = llistaCamio.get(i);
-					
-					/*A partir d'aquí, per pi, hem de fer swap amb totes les
-					 peticions que estan per sota d'ella. Anem a trobar
-					pj*/
-					
-					/*Del subcamió treurem pj*/
-					Camio subcamio = c;
-					ArrayList<Peticio> subllista = llistaCamio;
-					int filatmp = fila;
-					int isubllista = i+1;
+        /**
+         * L'estratègia a seguir serà la següent:
+         * Per un centre de producció CP
+         * 1. Per cada Peticio pi de CP, que es troba a HCP
+         *      Per totes les demés peticions pj tals que j > i (en ordre a la matriu HCP)
+         *        swap (pi,pj);
+         * 2. Per cada Petició pi de CP, que es troba a HCP
+         *      Per totes les peticions pj de la matriu endarrerits (en ordre a la matriu)
+         *	  swap (pi,pj);
+         *
+         * La condició de que j > i en (1), implica que no es repeteixen estats.
+         */
 
-					ArrayList<Peticio> endar = endarreritsPare.get(0,cp);
-					int iendar = 0;
-
-				        while (filatmp < Global.HORES_SERVEI)
-					    {	
-						subcamio = (Camio) hcpPare.getObj(filatmp,cp);
-						if (subcamio != null)
-						    {
-							subllista = subcamio.getLlistaPeticions();
-							
-							while (isubllista < subllista.size())
-							    {
-								Estat estatFill = new Estat(estatPare);
-								if (estatFill.swap(cp,fila,i,filatmp,isubllista))
-								    {
-									Successor suc = new Successor("",estatFill);
-									successors.add(suc);
-								    }
-								isubllista++;
-							    }
-						    }
-												
-						isubllista = 0;
-						filatmp++;
-					    }
-					/*Hem acabat de veure tots els Camions i peticions de HCP restants. Ara
-					  falta fer el swap per tots els endarrerits*/
-					while (iendar < endar.size())
-					    {
-						Estat estatFill = new Estat(estatPare);
-						if (estatFill.swapEndarrerits(cp,fila,i,iendar))
-						    {
-							Successor suc = new Successor("",estatFill);
-							successors.add(suc);
-						    }
-						iendar++;
-					    }				
-
-				    }
-			    }
-		    }
-	    }
-	return successors;
-    }
-}
-
-
-
-
-
-
-
-
-
-/*--------------------------------------8<-----------------------------------------------
-        //Generacio de successors: eliminar peticions, afegir peticions on capiguen, etc
-        //Generem estats treient peticions una a una
         for (int cp = 0; cp < Global.N_CENTRES; cp++) {
-            for (int h = 0; h < Global.HORES_SERVEI; h++) {
-                Camio camioActual = (Camio) camionsHCP.getObj(h,cp);
-                if(camioActual != null) {
-                    ArrayList <Peticio> llistaPeticions = camioActual.getLlistaPeticions();
-                    for(int pet = 0; pet < llistaPeticions.size(); pet++) {
-                        Peticio petActual = llistaPeticions.get(pet);
-                        Estat estatFill = treurePeticioDe(estatPare, h, cp, pet);
-                        int pesPet = petActual.getQuantitat();
-                        Successor suc = new Successor("treure Peticio de CP: " +
-                                                      (cp+1) + " hora: " + (8-h) + " pes: " + pesPet +
-                                                      estatFill.getValorsHeuristics(), estatFill);
-                        successors.add(suc);
+            for (int fila = 0; fila < Global.HORES_SERVEI; fila++) {
+                Camio c = (Camio) hcpPare.getObj(fila,cp);
+                if (c != null) {
+                    ArrayList<Peticio> llistaCamio = c.getLlistaPeticions();
+                    /*Del camio c, i de la i del seguent for, traurem el pi*/
+                    for (int i = 0; i < llistaCamio.size(); i++) {
+                        /*La petició pi és dins la HCP[fila][cp]->c->llistaCamio[i]*/
+                        Peticio pi = llistaCamio.get(i);
+
+                        /*A partir d'aquí, per pi, hem de fer swap amb totes les
+                         peticions que estan per sota d'ella. Anem a trobar
+                        pj*/
+
+                        /*Del subcamió treurem pj*/
+                        Camio subcamio = c;
+                        ArrayList<Peticio> subllista = llistaCamio;
+                        int filatmp = fila;
+                        int isubllista = i+1;
+
+                        ArrayList<Peticio> endar = endarreritsPare.get(0,cp);
+                        int iendar = 0;
+
+                        while (filatmp < Global.HORES_SERVEI) {
+                            subcamio = (Camio) hcpPare.getObj(filatmp,cp);
+                            if (subcamio != null) {
+                                subllista = subcamio.getLlistaPeticions();
+
+                                while (isubllista < subllista.size()) {
+                                    Estat estatFill = new Estat(estatPare);
+                                    if (estatFill.swap(cp,fila,i,filatmp,isubllista)) {
+                                        Successor suc = new Successor("",estatFill);
+                                        successors.add(suc);
+                                    }
+                                    isubllista++;
+                                }
+                            }
+
+                            isubllista = 0;
+                            filatmp++;
+                        }
+                        /*Hem acabat de veure tots els Camions i peticions de HCP restants. Ara
+                          falta fer el swap per tots els endarrerits*/
+                        while (iendar < endar.size()) {
+                            Estat estatFill = new Estat(estatPare);
+                            if (estatFill.swapEndarrerits(cp,fila,i,iendar)) {
+                                Successor suc = new Successor("",estatFill);
+                                successors.add(suc);
+                            }
+                            iendar++;
+                        }
+
                     }
                 }
             }
         }
-        //Generem estats afegint peticions no assignades (= endarrerides)
-        //únicament a camions on hi capiguen
-        Matriu peticionsEndarrerides = estatPare.getEndarrerits();
-        for(int cpEnd = 0; cpEnd < Global.N_CENTRES; cpEnd++) {
-            ArrayList<Peticio> llistaPeticions = peticionsEndarrerides.get(0,cpEnd);
-            for (int pet = 0; pet < llistaPeticions.size(); pet++) {
-                Peticio petActual = llistaPeticions.get(pet);
-                for(int hl = 0; hl < Global.HORES_SERVEI; hl++) {
-                    Camio camioActual = (Camio) camionsHCP.getObj(hl,cpEnd);
-                    //Si no hi havia camió
-                    if(camioActual == null) {
-                        //TODO
-                    }
-                    //Si  hi havia camió
-                    else {
-                        if(petActual.getQuantitat()+camioActual.getCarrega() <= camioActual.getTipus()) {
-                            Estat estatFill = possarPeticioA(estatPare, hl, cpEnd, petActual);
-                            int pesPet = petActual.getQuantitat();
-                            Successor suc = new Successor("possar Peticio a CP: " +
-                                                          (cpEnd+1) + " hora: " + (8-hl) + " pes: " + pesPet +
-                                                          estatFill.getValorsHeuristics(), estatFill);
-                            successors.add(suc);
-                        }
-                        //Generem estats afegint peticions modificant el tipus de camió si fa falta
-                        else {
-                            //TODO
-                        }
-                    }
-                }
-            }
-        }
-
-
-
         return successors;
     }
-
-    private Estat possarPeticioA(Estat estatPare, int hora, int cp, Peticio pet) {
-        Estat estatFill = new Estat(estatPare);
-        estatFill.afegirPeticio(hora, cp, pet);
-        return estatFill;
-    }
-
-    private Estat treurePeticioDe(Estat estatPare, int hora, int cp, int posicioPeticio) {
-        Estat estatFill = new Estat(estatPare);
-        estatFill.treurePeticio(hora, cp, posicioPeticio);
-        return estatFill;
-    }
-    }
-    ------------------------------------------->8-------------------------------------------*/
+}
